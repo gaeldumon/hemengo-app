9a749c4d774de364162fa7bcb96c9c9aa4b7a0e1
Forain <s.petit@algotech.fr>
2022-01-24 18:11:44 +0100

    Toast déconnexion - Ajoute un toast à la déconnexion

--- src/app/pages/logout/logout.page.ts
+++ src/app/pages/logout/logout.page.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TokenService } from 'src/app/services/token.service';
-
+import { toastController } from '@ionic/core';
 
 @Component({
     selector: 'app-logout',
     templateUrl: './logout.page.html',
     styleUrls: ['./logout.page.scss'],
 })
+
 export class LogoutPage implements OnInit {
 
     constructor(
-        private tokenService: TokenService
+        private tokenService: TokenService,
     ) { }
+
+    async toastSuccess(message: string, icon: string) {
+        const toast = await toastController.create({
+            color: 'success',
+            duration: 2000,
+            message: message,
+            icon: icon
+        });
+        await toast.present();
+    }
 
     ngOnInit() {
         this.tokenService.clearToken();
+        this.toastSuccess("Déconnexion réussie", 'log-out-outline');
     }
 
 }
