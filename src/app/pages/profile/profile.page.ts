cc9c09a890bc70b53723553c62cbecf5c1844a82
Forain <s.petit@algotech.fr>
2022-01-30 15:39:32 +0100

    Appel HemengoScanner - Ajoute l'appel à Hemengo Scanner lorsqu'on clique sur Récupérer commande

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -6,6 +6,8 @@ import { CityService } from 'src/app/services/city.service';
 import { OrderService } from 'src/app/services/order.service';
 import { VendingMachineService } from 'src/app/services/vending-machine.service';
 import { isToday } from 'src/app/helpers/util';
+import { Platform } from '@ionic/angular';
+import { toastController } from '@ionic/core'; //à supprimer, c'est juste pour les infos
 
 @Component({
     selector: 'app-profile',
@@ -21,7 +23,8 @@ export class ProfilePage implements OnInit {
         private cityService: CityService,
         private orderService: OrderService,
         private userService: UserService,
-        private vendingMachineService: VendingMachineService
+        private vendingMachineService: VendingMachineService,
+        private platform:Platform
     ) {
         this.orders = [];
         this.historicOrders = [];
@@ -87,5 +90,28 @@ export class ProfilePage implements OnInit {
         const d = new Date(date);
         const month = ((d.getMonth() + 1) < 9) ? "0" + (d.getMonth() + 1) : d.getMonth() + 1;
         return `${d.getDate()}/${month}/${d.getFullYear()}`;
+    }
+
+    //fonction à supprimer quand on aura plus besoin du toast
+    async toastSuccess(message: string, icon: string) {
+        const toast = await toastController.create({
+            color: 'success',
+            duration: 2000,
+            message: message,
+            icon: icon
+        });
+
+        await toast.present();
+    }
+
+    callHemengoScan() {
+        if (this.platform.is('android')) {
+            this.toastSuccess("on est sur android", 'log-out-outline');//à supprimer
+            window.open('android-app://com.example.hemengoscanner', "_system");//appelle l'appli hemengoScanner
+        } else if (this.platform.is('desktop')) {
+            this.toastSuccess('on est sur desktop', 'log-out-outline');//à supprimer
+        } else {
+            this.toastSuccess("on est ni sur android, ni sur desktop", 'log-out-outline')//à supprimer
+        }
     }
 }
