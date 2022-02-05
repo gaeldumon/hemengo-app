79d8df70cf828c80762bcb8501bd22caaec4e9f8
hyperdestru <slotuser@gmail.com>
2022-02-05 17:01:08 +0100

    Pickup order - Generate new app-scan component.

---
+++ src/app/components/app-scan/app-scan.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-app-scan',
+    templateUrl: './app-scan.component.html',
+    styleUrls: ['./app-scan.component.scss'],
+})
+export class AppScanComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
